276ce57260d1bf0768e0b75e88d25af45e9acf4f
pawel.kisielewicz@codepoets.it <pawel.kisielewicz@codepoets.it>
2019-04-24 15:28:18 +0200

    fixup! ffprobe_report tests: Unit tests for FfprobeFormatReport

--- tests/apps/ffmpeg/task/test_ffprobe_report.py
+++ tests/apps/ffmpeg/task/test_ffprobe_report.py
@@ -1,87 +1,72 @@
+import copy
 from unittest import TestCase
 
 from tests.apps.ffmpeg.task.ffprobe_report import FfprobeFormatReport
 from tests.apps.ffmpeg.task.ffprobe_report_sample_reports import \
-    RAW_REPORT_ORIGINAL, RAW_REPORT_TRANSCODED, \
-    RAW_REPORT_ORIGINAL_WITH_REMOVED_SUBTITLES, RAW_REPORT_WITH_MPEG4
+    RAW_REPORT_ORIGINAL, RAW_REPORT_WITH_MPEG4
 
 
 class TestFfprobeFormatReport(TestCase):
 
     def test_reports_with_shuffled_streams_should_be_compared_as_equal(self):
-        report_original = FfprobeFormatReport(
-            RAW_REPORT_ORIGINAL_WITH_REMOVED_SUBTITLES)
-        report_transcoded = FfprobeFormatReport(RAW_REPORT_TRANSCODED)
-        self.assertEqual(report_original, report_transcoded)
+        report_original = FfprobeFormatReport(RAW_REPORT_ORIGINAL)
+        raw_report_shuffled = copy.deepcopy(RAW_REPORT_ORIGINAL)
+
+        for stream in raw_report_shuffled['streams']:
+            if stream['index'] % 2 == 0:
+                stream['index'] = stream['index'] + 1
+            else:
+                stream['index'] = stream['index'] - 1
+
+        sorted(raw_report_shuffled['streams'], key=lambda i: i['index'])
+        assert raw_report_shuffled != RAW_REPORT_ORIGINAL
+        report_shuffled = FfprobeFormatReport(raw_report_shuffled)
+
+        self.assertEqual(report_original, report_shuffled)
 
     def test_missing_streams_should_be_reported(self):
-        report_original = FfprobeFormatReport(RAW_REPORT_ORIGINAL)
-        report_transcoded = FfprobeFormatReport(RAW_REPORT_TRANSCODED)
-        diff = (report_transcoded.diff(report_original))
+        raw_report_original = copy.deepcopy(RAW_REPORT_ORIGINAL)
+        del raw_report_original['streams'][2]
+        report_original = FfprobeFormatReport(raw_report_original)
 
+        raw_report_modified = copy.deepcopy(RAW_REPORT_ORIGINAL)
+        del raw_report_modified['streams'][10]
+        del raw_report_modified['streams'][9]
+        report_modified = FfprobeFormatReport(raw_report_modified)
+
+        diff = report_modified.diff(report_original)
         expected_diff = [
             {
                 'location': 'format',
                 'attribute': 'stream_types',
                 'original_value': {
                     'audio': 2,
-                    'video': 1
+                    'video': 1,
+                    'subtitle': 6
                 },
                 'modified_value': {
                     'video': 1,
                     'audio': 2,
-                    'subtitle': 8
+                    'subtitle': 7
                 },
                 'reason': 'Different attribute values'
             },
             {
                 'location': 'subtitle',
-                'original_stream_index': None,
-                'modified_stream_index': 0,
-                'reason': 'No matching stream'
-            },
-            {
-                'location': 'subtitle',
-                'original_stream_index': None,
-                'modified_stream_index': 1,
-                'reason': 'No matching stream'
-            },
-            {
-                'location': 'subtitle',
-                'original_stream_index': None,
-                'modified_stream_index': 2,
-                'reason': 'No matching stream'
-            },
-            {
-                'location': 'subtitle',
-                'original_stream_index': None,
+                'attribute': 'language',
+                'original_value': 'eng',
+                'modified_value': 'hun',
+                'reason': 'Different attribute values',
+                'original_stream_index': 2,
                 'modified_stream_index': 3,
-                'reason': 'No matching stream'
+
             },
             {
                 'location': 'subtitle',
                 'original_stream_index': None,
-                'modified_stream_index': 4,
+                'modified_stream_index': 9,
                 'reason': 'No matching stream'
             },
-            {
-                'location': 'subtitle',
-                'original_stream_index': None,
-                'modified_stream_index': 5,
-                'reason': 'No matching stream'
-            },
-            {
-                'location': 'subtitle',
-                'original_stream_index': None,
-                'modified_stream_index': 6,
-                'reason': 'No matching stream'
-            },
-            {
-                'location': 'subtitle',
-                'original_stream_index': None,
-                'modified_stream_index': 7,
-                'reason': 'No matching stream'
-            }
         ]
         self.assertCountEqual(diff, expected_diff)
 
